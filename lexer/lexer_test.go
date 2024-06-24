0a8caa9b3d333e4f13675737adb8b133ac509467
Yoshihiro Koyama <h2so4@mineo.jp>
2024-06-24 09:57:23 +0900

    tokenize chars

---
+++ lexer/lexer_test.go
@@ -0,0 +1,42 @@
+package lexer_test
+
+import (
+	"testing"
+
+	"github.com/yox5ro/monkey/lexer"
+	"github.com/yox5ro/monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	t.Parallel()
+
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := lexer.New(input)
+
+	for i, tt := range tests {
+		tt := tt
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
